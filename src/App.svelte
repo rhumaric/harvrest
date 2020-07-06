c77d7225a38b005387f1056ef4176cfa8cac0743
Romaric Pascal <hello@romaricpascal.is>
2020-07-06 18:08:16 +0100

    feat: swap between active and rest sessions

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,47 @@
 <script>
   import Duration from './Duration.svelte';
   import { time } from './stores.js';
+
+  let started;
+  let rest;
+  let elapsed = 0;
+  // Initialize with a no-op so there's no need
+  // to check if it holds an unsubscribe function
+  // before calling it
+  let stop = () => {};
+  let threshold = 5 * 1000;
+  let restTime = 1 * 1000;
+
+  function endSession() {
+    stop();
+    if (!rest && elapsed > threshold) {
+      rest = true;
+      startTimer();
+    } else {
+      started = false;
+      rest = false;
+    }
+  }
+  function startSession() {
+    started = true;
+    startTimer();
+  }
+  function startTimer() {
+    // Subscribe to the store ourselves
+    // so we can unsubscribe on our own time
+    // rather than when Svelte destroys the Duration
+    // subscription
+    stop = time().subscribe(registerTime);
+  }
+  function registerTime(value) {
+    elapsed = value;
+  }
 </script>
 
-<Duration duration={$time / 1000} />
+{#if started}
+  {rest ? 'Rest' : 'Active'}
+  <Duration duration={elapsed / 1000} />
+  <button on:click={endSession}>End</button>
+{:else}
+  <button on:click={startSession}>Start</button>
+{/if}
--- src/stores.js
+++ src/stores.js
@@ -1,12 +1,14 @@
 import { readable } from 'svelte/store';
 
-export const time = readable(0, function start(set) {
-  const start = new Date();
-  const interval = setInterval(() => {
-    set(new Date() - start);
-  }, 1000);
+export function time() {
+  return readable(0, function start(set) {
+    const start = new Date();
+    const interval = setInterval(() => {
+      set(new Date() - start);
+    }, 1000);
 
-  return function stop() {
-    clearInterval(interval);
-  };
-});
+    return function stop() {
+      clearInterval(interval);
+    };
+  });
+}
