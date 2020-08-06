131a1000f5d846768da09b512d5d69d0138d2756
Romaric Pascal <hello@romaricpascal.is>
2020-08-06 18:11:01 +0100

    feat: use hash in URL to navigate to settings

--- src/screens/Home.svelte
+++ src/screens/Home.svelte
@@ -7,23 +7,34 @@
   export let settings;
   export let action = () => {};
 
-  let configure = false;
+  let hash = window.location.hash;
+
+  function updateHash() {
+    hash = window.location.hash;
+  }
+
+  function onSubmit() {
+    action();
+    window.location.hash = '';
+  }
 
   onMount(() => {
     page();
+    window.addEventListener('hashchange', updateHash);
+    return () => {
+      window.removeEventListener('hashchange', updateHash);
+    };
   });
 </script>
 
-<form aria-label="Settings" class="content" on:submit|preventDefault={action}>
-  {#if configure}
+<form aria-label="Settings" class="content" on:submit|preventDefault={onSubmit}>
+  {#if hash === '#/settings'}
     <Settings {settings} />
   {:else}
     <p class="lead">Pomodoro, with a twist!</p>
     <Description {settings} />
     <p>
-      <a href="#settings" on:click|preventDefault={() => (configure = true)}>
-        Set your own times
-      </a>
+      <a href="#/settings">Set your own times</a>
     </p>
   {/if}
   <button class="content__action">Let's go!</button>
