a4fb9c3797b12829273876f5786a2759424bbb63
Romaric Pascal <hello@romaricpascal.is>
2020-08-10 17:08:14 +0100

    feat: ensure only end notification plays if simultaneous with threshold

--- src/Notifications.svelte
+++ src/Notifications.svelte
@@ -11,7 +11,7 @@
   export let end;
 </script>
 
-{#if threshold && Number.isFinite(threshold)}
+{#if threshold && Number.isFinite(threshold) && threshold !== end}
   <Notification
     url={thresholdUrl}
     trigger={threshold}
