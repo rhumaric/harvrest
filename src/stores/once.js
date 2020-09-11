00c70cf6e9423af2effd6fd012c962635ccfafcf
Romaric Pascal <hello@romaricpascal.is>
2020-09-11 16:31:51 +0100

    feat: fix unsubscription issues in once

--- src/stores/once.js
+++ src/stores/once.js
@@ -7,6 +7,7 @@ export function once(stores, derive, initial) {
     function(...args) {
       const result = derive(...args);
       if (typeof result !== 'undefined') {
+        // Await next tick so every subsriber is correctly notified
         tick().then(done);
       }
       return result;
@@ -14,18 +15,45 @@ export function once(stores, derive, initial) {
     initial
   );
 
-  let stop;
+  // Keep track of all unsubscribers
+  // so we can cleanly handler unsubscriptions
+  let unsubscribers = [];
+
   return {
     subscribe(fn) {
-      stop = subscribe(fn);
-      return done;
+      // First grab the unsubscription function
+      let unsubscribe = subscribe(fn);
+
+      // Because unsubscription can also happen outside
+      // of our control and calling twice throws an error
+      // we need to have our own unsubscribe function
+      // that keeps track of whether we've already unsubscribed
+      // or not
+      unsubscribers.push(doUnsubscribe);
+
+      // If the unsubscription comes from outside
+      // we can remote the unsubscriber from the list
+      // so there's less calls when stopping
+      const index = unsubscribers.length;
+      return function() {
+        unsubscribers.splice(index);
+        doUnsubscribe();
+      };
+
+      function doUnsubscribe() {
+        if (unsubscribe) {
+          unsubscribe();
+          unsubscribe = null;
+        }
+      }
     }
   };
 
   function done() {
-    if (stop) {
+    for (const stop of unsubscribers) {
+      console.log(stop);
       stop();
-      stop = null;
     }
+    unsubscribers = [];
   }
 }
