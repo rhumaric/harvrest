93549924a5c5e24ed80185c00778e70d9cc56c8e
Romaric Pascal <hello@romaricpascal.is>
2020-10-21 15:00:23 +0100

    feat: update copy of intro description

--- src/screens/Home/Description.svelte
+++ src/screens/Home/Description.svelte
@@ -9,11 +9,12 @@
 
 <p />
 <p>
-  As usual, after {formatTime(settings.minActiveTime)} of activity, grab some well
-  deserved {formatTime(settings.restForMinActiveTime)} of rest.
+  Like in a regular pomodorro, break down your work in chunks of active and rest
+  time. After {formatTime(settings.minActiveTime)} of activity, grab some well deserved
+  {formatTime(settings.restForMinActiveTime)} of rest.
 </p>
 <p>
-  If you're in the middle of something when the time is up, you can carry on (up
-  to a total of {formatTime(settings.maxActiveTime)} of activity)! <strong>The
-    rest time will grow accordingly!</strong>
+  But, if you're in the middle of something when the time is up, feel free to
+  can carry on (up to a total of {formatTime(settings.maxActiveTime)} of activity)!
+  <strong>The rest time will grow accordingly!</strong>
 </p>
